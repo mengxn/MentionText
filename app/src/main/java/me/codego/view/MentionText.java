47ecd82b8bf152661a1b53adf298fabd7d206583
mengxn <mengxn@outlook.com>
2016-10-13 22:33:05 +0800

    remove previous spans

--- app/src/main/java/me/codego/view/MentionText.java
+++ app/src/main/java/me/codego/view/MentionText.java
@@ -48,17 +48,21 @@ public class MentionText {
         }
         textView.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
             }
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
 
             }
 
             @Override
             public void afterTextChanged(Editable editable) {
+                ForegroundColorSpan[] oldSpans = editable.getSpans(0, editable.length(), ForegroundColorSpan.class);
+                for (ForegroundColorSpan oldSpan : oldSpans) {
+                    editable.removeSpan(oldSpan);
+                }
                 Pattern pattern = Pattern.compile(regex);
                 Matcher matcher = pattern.matcher(editable.toString());
                 while (matcher.find()) {
