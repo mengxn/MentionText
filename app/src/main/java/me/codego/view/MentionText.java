1d3bb49c42760356c946942ae57c825fb95b4abc
mengxn <mengxn@outlook.com>
2016-10-25 23:35:36 +0800

    fix bug of delete @

--- app/src/main/java/me/codego/view/MentionText.java
+++ app/src/main/java/me/codego/view/MentionText.java
@@ -7,10 +7,14 @@ import android.text.style.ForegroundColorSpan;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
+ *
  * Created by mengxn on 16/10/8.
  */
 
@@ -19,11 +23,13 @@ public class MentionText {
     private String regex;
     private int color;
 
-    private static final String DEFAULT_PATTERN = "[@|#]\\w+";
+    private List<Range> rangeList = new ArrayList<>();
+
+    private static final String DEFAULT_PATTERN = "@|#";
     private static final int DEFAULT_COLOR = 0xffff0000;
 
     private MentionText(String regex, int color) {
-        this.regex = regex;
+        this.regex = String.format(Locale.getDefault(), "[%s]\\w+", regex);
         this.color = color;
     }
 
@@ -35,7 +41,7 @@ public class MentionText {
         return from(regex, DEFAULT_COLOR);
     }
 
-    public static MentionText from(int color) {
+    public static MentionText FROM(int color) {
         return from(DEFAULT_PATTERN, color);
     }
 
@@ -48,16 +54,18 @@ public class MentionText {
             return;
         }
         textView.addTextChangedListener(new TextWatcher() {
+
+            private Range readyDeleteRange;
+
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
                 if (textView instanceof EditText) {
                     if (count == 1 && after == 0 && s.charAt(start) == ' ') {
-                        String str = s.toString();
-                        int index = str.lastIndexOf('@', start);
-                        if (index >= 0) {
-                            str = str.substring(0, index) + str.substring(start + 1);
-                            textView.setText(str);
-                            ((EditText) textView).setSelection(index);
+                        for (Range range : rangeList) {
+                            if (range.end == start) {
+                                readyDeleteRange = range;
+                                break;
+                            }
                         }
                     }
                 }
@@ -70,14 +78,24 @@ public class MentionText {
 
             @Override
             public void afterTextChanged(Editable editable) {
+                if (readyDeleteRange != null) {
+                    int start = readyDeleteRange.start;
+                    int end = readyDeleteRange.end;
+                    readyDeleteRange = null;
+                    editable.delete(start, end);
+                }
                 ForegroundColorSpan[] oldSpans = editable.getSpans(0, editable.length(), ForegroundColorSpan.class);
                 for (ForegroundColorSpan oldSpan : oldSpans) {
                     editable.removeSpan(oldSpan);
                 }
                 Pattern pattern = Pattern.compile(regex);
                 Matcher matcher = pattern.matcher(editable.toString());
+                rangeList.clear();
                 while (matcher.find()) {
-                    editable.setSpan(new ForegroundColorSpan(color), matcher.start(), matcher.end(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+                    int start = matcher.start();
+                    int end = matcher.end();
+                    rangeList.add(new Range(start, end));
+                    editable.setSpan(new ForegroundColorSpan(color), start, end, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
                 }
             }
         });
---
+++ app/src/main/java/me/codego/view/Range.java
@@ -0,0 +1,15 @@
+package me.codego.view;
+
+/**
+ * Created by mengxn on 2016/10/25.
+ */
+
+public class Range {
+    int start;
+    int end;
+
+    public Range(int start, int end) {
+        this.start = start;
+        this.end = end;
+    }
+}
